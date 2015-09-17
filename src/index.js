86d690c025c3cc28b2f13779dc477adfcbc7c950
Andrew Shaffer <darawk@gmail.com>
2015-09-17 00:14:53 -0700

    add some comments

--- src/index.js
+++ src/index.js
@@ -10,26 +10,39 @@ import {diff, patch, create} from 'virtual-dom'
  */
 
 function vdux (store, app, node) {
-  store.subscribe(update)
+  /**
+   * Render the VDOM tree
+   */
 
   let tree = render()
   let rootNode = create(tree)
 
   node.appendChild(rootNode)
 
-  // Set delegator on the node *above* our actual node
-  // so that it doesn't change
+  /**
+   * Create the Virtual DOM <-> Redux cycle
+   */
+
   delegate(node, store.dispatch)
+  store.subscribe(update)
+
+  /**
+   * Render a new virtual dom
+   */
+
+  function render () {
+    return app(store.getState())
+  }
+
+  /**
+   * Sync the virtual dom and the actual dom
+   */
 
   function update () {
     const newTree = render()
 
     rootNode = patch(rootNode, diff(tree, newTree))
     tree = newTree
-  }
-
-  function render () {
-    return app(store.getState())
   }
 }
 
